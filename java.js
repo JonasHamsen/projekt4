4fb19c93b7e2f0470d95a50b456c183e737b2d89
DrCones <jonas98@live.dk>
2021-02-21 22:48:02 +0100

    Revert "Create java.js"
    This reverts commit 8938042d0bf5bef4415cb33662a40509441a7203.

--- java.js
+++
@@ -1,9 +0,0 @@
-
-var cars = ["Saab", "Volvo", "BMW"];
-document.getElementById("demo").innerHTML = cars[0];
-
-if (cars==0) {
-  alert("its a car!");
-  document.getElementById("demo2").innerHTML;
-}
-console.log(cars);
