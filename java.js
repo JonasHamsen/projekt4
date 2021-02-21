8938042d0bf5bef4415cb33662a40509441a7203
DrCones <jonas98@live.dk>
2021-02-21 22:46:53 +0100

    Create java.js

---
+++ java.js
@@ -0,0 +1,9 @@
+
+var cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars[0];
+
+if (cars==0) {
+  alert("its a car!");
+  document.getElementById("demo2").innerHTML;
+}
+console.log(cars);
